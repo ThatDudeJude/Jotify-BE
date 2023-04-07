11dd9cedf5e1b22c7ef08038d729780ca265b344
ThatDudeJude <judegachoki@gmail.com>
2023-04-07 14:32:55 +0300

    Added versioning

--- notable/urls.py
+++ notable/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/", include("base.urls")),
+    path("api/v1/auth/", include("base.urls")),
     path("api-auth/", include(("rest_framework.urls"))),
 ]
