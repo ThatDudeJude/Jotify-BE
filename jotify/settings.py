ae6bec713a4c915fc2429f02e6073b14df8a7267
ThatDudeJude <judegachoki@gmail.com>
2024-04-29 11:38:53 +0300

    Added BACKENDHOST to allowed hosts

--- jotify/settings.py
+++ jotify/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [env.str("HOSTNAME1"), env.str("NETWORKHOST"), "127.0.0.1"]
+ALLOWED_HOSTS = [env.str("HOSTNAME1"), env.str("BACKENDHOST"), "127.0.0.1"]
 
 
 # Application definition
